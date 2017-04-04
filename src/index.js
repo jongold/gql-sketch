de005421ba4f4c01675f05354384876752d0fa3f
Jon Gold <hello@designedbygold.com>
2017-04-04 13:01:06 -0700

    switch from sketchfetch to sketch-module-fetch-polyfill

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import fetch from 'sketchfetch';
+import fetch from 'sketch-module-fetch-polyfill';
 
 const Client = endpoint => ({
   query: q =>
     fetch(endpoint, {
       method: 'POST',
       body: {
-        query: q.replace(/\s/g, ''),
+        query: q.replace(/(\s\n)/g, ''),
       },
-    }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }).then(res => res.json()).then(json => json.data),
 });
 
 export default Client;
